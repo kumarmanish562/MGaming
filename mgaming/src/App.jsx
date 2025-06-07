cc99c6927ef707ec309d9ef5df08a1859cc181b7
kumarmanish562 <qmanishkumar562@gmail.com>
2025-06-07 16:14:46 +0530

    Adds Story component to main application view
    Integrates the Story component into the main application layout, enhancing the user interface by providing additional content.
    
    This change improves the overall functionality of the app by expanding its features and user engagement options.

--- mgaming/src/App.jsx
+++ mgaming/src/App.jsx
@@ -3,6 +3,7 @@ import About from "./components/About";
 import Features from "./components/Features";
 import Hero from "./components/Hero";
 import NavBar from "./components/Navbar";
+import Story from "./components/Story";
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Hero />
       <About />
       <Features />
+      <Story />
      
     </main>
   );

2519e57de1a66fd5517b92121af3a27654ec6152
kumarmanish562 <qmanishkumar562@gmail.com>
2025-06-07 17:32:44 +0530

    Updates project name to 'awwwards-website' in package.json; removes unnecessary build configurations from vite.config.js

--- mgaming/vite.config.js
+++ mgaming/vite.config.js
@@ -4,17 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/mgaming/', // Replace 'mgaming' with your repository name
-  build: {
-    outDir: 'dist',
-    sourcemap: false,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom'],
-          gsap: ['gsap']
-        }
-      }
-    }
-  }
 })

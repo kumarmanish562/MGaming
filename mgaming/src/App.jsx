7bdc1431a427eeb3faf5e822f8a95150470f054b
kumarmanish562 <qmanishkumar562@gmail.com>
2025-06-07 16:18:50 +0530

    Adds Footer component to main application layout
    Integrates the Footer component into the main application structure, enhancing the overall user interface and providing additional information at the bottom of the page.
    
    This change improves the user experience by ensuring essential links and information are readily accessible.

--- mgaming/src/App.jsx
+++ mgaming/src/App.jsx
@@ -2,6 +2,7 @@
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Features from "./components/Features";
+import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import NavBar from "./components/Navbar";
 import Story from "./components/Story";
@@ -16,6 +17,7 @@ function App() {
       <Features />
       <Story />
       <Contact />
+      <Footer />
      
     </main>
   );
